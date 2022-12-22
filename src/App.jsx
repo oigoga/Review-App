6b363a3c5748896c76806322b03005a7e3f3d8f6
oigoga <gogaelisabeth21@gmail.com>
2022-12-22 14:30:59 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </div>
   )
 }
@@ -36,10 +17,4 @@ export default App
 
 
 
-echo "# Review-App" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/oigoga/Review-App.git
-git push -u origin main
+
