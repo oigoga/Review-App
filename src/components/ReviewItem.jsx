778c49f2e02e652a685e9eae2ae0215bf36e7460
oigoga <gogaelisabeth21@gmail.com>
2023-01-03 12:06:07 +0100

    I edited the colours in the css

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,20 +1,13 @@
 import { useState } from "react";
 
 function ReviewItem() {
-  const [rating, setRating] = useState(6);
-  const [text, setText] = useState("This is review text mm");
-
- const btnChanged= () => {
-     setRating = () => {
-        return 16
-    }
-  }
+ 
 
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={btnChanged}>Submit</button>
+      <div className="num-display">10</div>
+      <div className="text-display">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+      
     </div>
   );
 }
